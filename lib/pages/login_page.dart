c85da2a39e20abf0764fb6829722714bc35daf4a
padfoot-16 <amigo_awini@hotmail.com>
2023-07-29 03:05:56 +0100

    overflow fixed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -21,68 +21,71 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              //logo
-              Container(
-                height: 200,
-                child: Image.asset("assets/pulse.png"),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //welcome back message
-              Text(
-                "Welcome back you've been missed!",
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              //email textfield
-              MyTextfield(
-                  controller: emailcontroller,
-                  hinttext: "put your email here ",
-                  obscuretext: false),
-
-              SizedBox(
-                height: 15,
-              ),
-              //password textfield
-              MyTextfield(
-                  controller: passwordcontroller,
-                  hinttext: "Put your password here",
-                  obscuretext: true),
-
-              SizedBox(
-                height: 25,
-              ),
-              //sign in button
-              MyButton(
-              onTap: signIn,
-               text: "Sign In"),
-
-               SizedBox(height: 50,),
-              //register
-              Row(
+      body: SingleChildScrollView(
+        physics: ClampingScrollPhysics(),
+        child: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:[
-                  Text("Not a member ?"),
-                  SizedBox(width: 5,),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Text("Register Now",style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepPurple[800]),),
-                  )
-                ])
-            ]),
+                children: [
+                //logo
+                Container(
+                  height: 200,
+                  child: Image.asset("assets/pulse.png"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                //welcome back message
+                Text(
+                  "Welcome back you've been missed!",
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+      
+                //email textfield
+                MyTextfield(
+                    controller: emailcontroller,
+                    hinttext: "put your email here ",
+                    obscuretext: false),
+      
+                SizedBox(
+                  height: 15,
+                ),
+                //password textfield
+                MyTextfield(
+                    controller: passwordcontroller,
+                    hinttext: "Put your password here",
+                    obscuretext: true),
+      
+                SizedBox(
+                  height: 25,
+                ),
+                //sign in button
+                MyButton(
+                onTap: signIn,
+                 text: "Sign In"),
+      
+                 SizedBox(height: 50,),
+                //register
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[
+                    Text("Not a member ?"),
+                    SizedBox(width: 5,),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: Text("Register Now",style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepPurple[800]),),
+                    )
+                  ])
+              ]),
+            ),
           ),
         ),
       ),
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -24,77 +24,80 @@ class _RegistrePageState extends State<RegistrePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              //logo
-              Container(
-                height: 200,
-                child: Image.asset("assets/pulse.png"),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //welcome back message
-              Text(
-                "Let's create an account for you!",
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              //email textfield
-              MyTextfield(
-                  controller: emailcontroller,
-                  hinttext: "put your email here ",
-                  obscuretext: false),
-
-              SizedBox(
-                height: 15,
-              ),
-              //password textfield
-              MyTextfield(
-                  controller: passwordcontroller,
-                  hinttext: "Put your password here",
-                  obscuretext: true),
-
-              SizedBox(
-                height: 25,
-              ),
-              //confirm password textfield
-              MyTextfield(
-                  controller: confirmpasswordcontroller,
-                  hinttext: "Confirm your password",
-                  obscuretext: true),
-
-              SizedBox(
-                height: 25,
-              ),
-              //sign in button
-              MyButton(
-              onTap: signUp,
-               text: "Sign Up"),
-
-               SizedBox(height: 50,),
-              //register
-              Row(
+      body: SingleChildScrollView(
+        physics: ClampingScrollPhysics(),
+        child: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:[
-                  Text("Already a member ?"),
-                  SizedBox(width: 5,),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Text("Login",style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepPurple[800]),),
-                  )
-                ])
-            ]),
+                children: [
+                //logo
+                Container(
+                  height: 200,
+                  child: Image.asset("assets/pulse.png"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                //welcome back message
+                Text(
+                  "Let's create an account for you!",
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+      
+                //email textfield
+                MyTextfield(
+                    controller: emailcontroller,
+                    hinttext: "put your email here ",
+                    obscuretext: false),
+      
+                SizedBox(
+                  height: 15,
+                ),
+                //password textfield
+                MyTextfield(
+                    controller: passwordcontroller,
+                    hinttext: "Put your password here",
+                    obscuretext: true),
+      
+                SizedBox(
+                  height: 25,
+                ),
+                //confirm password textfield
+                MyTextfield(
+                    controller: confirmpasswordcontroller,
+                    hinttext: "Confirm your password",
+                    obscuretext: true),
+      
+                SizedBox(
+                  height: 25,
+                ),
+                //sign in button
+                MyButton(
+                onTap: signUp,
+                 text: "Sign Up"),
+      
+                 SizedBox(height: 50,),
+                //register
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[
+                    Text("Already a member ?"),
+                    SizedBox(width: 5,),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: Text("Login",style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepPurple[800]),),
+                    )
+                  ])
+              ]),
+            ),
           ),
         ),
       ),
